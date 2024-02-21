883cc5cdbd378cd117348ce28dff920e7b002c4f
fernando-pulido <fernandopulido95@icloud.com>
2024-02-20 22:10:32 -0500

    The index file was edited and the reactstrap library was added

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
 import { useState } from 'react'
+import './App.css'
+import {} from './components/form/type'
 
+import { mockToDoList } from './mocks/todo-mock'
 import Form from './components/form/InputForm'
 
-import { mockToDoList as initialData } from './mocks/todo-mock'
-import type { Mock } from './mocks/todo-mock'
-
-import './App.css'
 function App() {
-  const [todo] = useState<Mock[]>(initialData)
+  const [todos] = useState(mockToDoList)
 
   return (
     <div className="main-container">
       <h1>To-do-list</h1>
       <div className="container-form">
-        <Form todo={todo} />
+        <Form todos={todos} />
       </div>
     </div>
   )
